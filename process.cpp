b09bbcfac934a62dfb021bbbe3616526b884d836
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-04-19 00:43:17 +0900

    Update nvram_flash option for consistency

--- process.cpp
+++ process.cpp
@@ -536,7 +536,6 @@ std::vector<FlashStep> BuildFlashSteps() {
     };
 
     if (opt("pgpt")) steps.push_back({FB("flash partition " + Img("pgpt.img")), L"PGPT 書き込み中…", "pgpt.img"});
-    if (opt("nvram_flash1")) steps.push_back({FB("flash nvram " + Img("nvram.img")), L"NVRAM 書き込み中…", "nvram.img"});
     if (opt("nvcfg")) steps.push_back({FB("flash nvcfg " + Img("nvcfg.img")), L"NVCFG 書き込み中…", "nvcfg.img"});
     if (opt("nvdata")) steps.push_back({FB("flash nvdata " + Img("nvdata.img")), L"NVDATA 書き込み中…", "nvdata.img"});
     if (opt("persist")) steps.push_back({FB("flash persist " + Img("persist.img")), L"PERSIST 書き込み中…", "persist.img"});
@@ -548,7 +547,7 @@ std::vector<FlashStep> BuildFlashSteps() {
     if (opt("protect1_erase")) steps.push_back({FB("erase protect1"), L"PROTECT1 消去中…", nullptr});
     if (opt("protect2_erase")) steps.push_back({FB("erase protect2"), L"PROTECT2 消去中…", nullptr});
     if (opt("nvram_erase")) steps.push_back({FB("erase nvram"), L"NVRAM 消去中…", nullptr});
-    if (opt("nvram_flash2")) steps.push_back({FB("flash nvram " + Img("nvram.img")), L"NVRAM 書き込み中…", "nvram.img"});
+    if (opt("nvram_flash")) steps.push_back({FB("flash nvram " + Img("nvram.img")), L"NVRAM 書き込み中…", "nvram.img"});
     if (opt("lk")) steps.push_back({FB("flash lk " + Img("lk.img")), L"LK 書き込み中…", "lk.img"});
     if (opt("lk2")) steps.push_back({FB("flash lk2 " + Img("lk2.img")), L"LK2 書き込み中…", "lk2.img"});
     if (opt("boot")) steps.push_back({FB("flash boot " + Img("boot.img")), L"BOOT 書き込み中…", "boot.img"});
