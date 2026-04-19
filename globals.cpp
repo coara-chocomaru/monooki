ca2efc735176b15b2ecfeb1e43be5d2a91f5a689
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-04-19 16:38:25 +0900

    Add color constants and new HWND variables

--- globals.cpp
+++ globals.cpp
@@ -2,9 +2,27 @@
 
 const wchar_t APP_CLASS[] = L"A05BDFlasher";
 
+COLORREF C_BG       = RGB(15, 18, 28);
+COLORREF C_BG2      = RGB(21, 26, 40);
+COLORREF C_PANEL    = RGB(25, 31, 48);
+COLORREF C_PANEL2   = RGB(30, 38, 58);
+COLORREF C_LINE     = RGB(56, 69, 98);
+COLORREF C_TEXT     = RGB(237, 242, 247);
+COLORREF C_MUTED    = RGB(155, 168, 190);
+COLORREF C_ACCENT   = RGB(0, 179, 255);
+COLORREF C_SUCCESS  = RGB(52, 199, 89);
+COLORREF C_WARNING  = RGB(255, 185, 0);
+COLORREF C_DANGER   = RGB(255, 92, 92);
+COLORREF C_BTN      = RGB(39, 49, 72);
+COLORREF C_BTN_HOV  = RGB(52, 63, 92);
+COLORREF C_BTN_DN   = RGB(26, 37, 60);
+COLORREF C_BTN_EDGE = RGB(86, 102, 138);
+COLORREF C_EDIT_BG  = RGB(17, 21, 31);
+
 HWND g_hMain{};
 HWND hBtnCheck{};
 HWND hBtnFlash{};
+HWND hBtnSettings{};
 HWND hProgressBar{};
 HWND hLog{};
 HWND hLblStatus{};
@@ -42,3 +60,5 @@ Layout g_layout{};
 std::mutex g_LogMutex;
 std::deque<LogLine> g_LogQueue;
 std::atomic<bool> g_LogFlushPending{false};
+
+Config g_Config = {0, L"", L""};
