9001087b1daadd624eb577aa48283d9af9b4fea3
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-04-19 16:37:43 +0900

    Update app.h

--- app.h
+++ app.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#define NOMINMAX
 #include <windows.h>
 #include <commctrl.h>
+#include <gdiplus.h>
 
 #include <atomic>
 #include <deque>
@@ -9,46 +11,50 @@
 #include <string>
 #include <vector>
 
+#pragma comment(lib, "gdiplus.lib")
+#pragma comment(lib, "comctl32.lib")
+
 #ifndef EM_SETBKGNDCOLOR
 #define EM_SETBKGNDCOLOR 0x0443
 #endif
 
-constexpr int ID_BTN_CHECK  = 101;
-constexpr int ID_BTN_FLASH  = 102;
-constexpr int ID_PROGRESS   = 103;
-constexpr int ID_LOG_WINDOW = 104;
-constexpr int ID_LBL_STATUS = 105;
-constexpr int ID_LBL_DEVICE = 106;
-constexpr int ID_LBL_FASTBT = 107;
-constexpr int ID_LBL_ROM    = 108;
-constexpr int ID_LBL_STEPS  = 109;
-constexpr int ID_LBL_HINT   = 110;
-constexpr int ID_LBL_TITLE  = 111;
-constexpr int ID_LBL_SUB    = 112;
+constexpr int ID_BTN_CHECK    = 101;
+constexpr int ID_BTN_FLASH    = 102;
+constexpr int ID_PROGRESS     = 103;
+constexpr int ID_LOG_WINDOW   = 104;
+constexpr int ID_LBL_STATUS   = 105;
+constexpr int ID_LBL_DEVICE   = 106;
+constexpr int ID_LBL_FASTBT   = 107;
+constexpr int ID_LBL_ROM      = 108;
+constexpr int ID_LBL_STEPS    = 109;
+constexpr int ID_LBL_HINT     = 110;
+constexpr int ID_LBL_TITLE    = 111;
+constexpr int ID_LBL_SUB      = 112;
+constexpr int ID_BTN_SETTINGS = 113;
 
 constexpr UINT WM_LOG_FLUSH = WM_APP + 1;
 constexpr UINT WM_PROG_SET  = WM_APP + 2;
 constexpr UINT WM_OP_DONE   = WM_APP + 3;
-constexpr UINT WM_TEXT_SET   = WM_APP + 4;
+constexpr UINT WM_TEXT_SET  = WM_APP + 4;
 
 extern const wchar_t APP_CLASS[];
 
-constexpr COLORREF C_BG       = RGB(15, 18, 28);
-constexpr COLORREF C_BG2      = RGB(21, 26, 40);
-constexpr COLORREF C_PANEL    = RGB(25, 31, 48);
-constexpr COLORREF C_PANEL2   = RGB(30, 38, 58);
-constexpr COLORREF C_LINE     = RGB(56, 69, 98);
-constexpr COLORREF C_TEXT     = RGB(237, 242, 247);
-constexpr COLORREF C_MUTED    = RGB(155, 168, 190);
-constexpr COLORREF C_ACCENT   = RGB(0, 179, 255);
-constexpr COLORREF C_SUCCESS  = RGB(52, 199, 89);
-constexpr COLORREF C_WARNING  = RGB(255, 185, 0);
-constexpr COLORREF C_DANGER   = RGB(255, 92, 92);
-constexpr COLORREF C_BTN      = RGB(39, 49, 72);
-constexpr COLORREF C_BTN_HOV  = RGB(52, 63, 92);
-constexpr COLORREF C_BTN_DN   = RGB(26, 37, 60);
-constexpr COLORREF C_BTN_EDGE  = RGB(86, 102, 138);
-constexpr COLORREF C_EDIT_BG   = RGB(17, 21, 31);
+extern COLORREF C_BG;
+extern COLORREF C_BG2;
+extern COLORREF C_PANEL;
+extern COLORREF C_PANEL2;
+extern COLORREF C_LINE;
+extern COLORREF C_TEXT;
+extern COLORREF C_MUTED;
+extern COLORREF C_ACCENT;
+extern COLORREF C_SUCCESS;
+extern COLORREF C_WARNING;
+extern COLORREF C_DANGER;
+extern COLORREF C_BTN;
+extern COLORREF C_BTN_HOV;
+extern COLORREF C_BTN_DN;
+extern COLORREF C_BTN_EDGE;
+extern COLORREF C_EDIT_BG;
 
 struct ExecResult {
     DWORD exitCode{};
@@ -80,9 +86,16 @@ struct Layout {
     RECT logCard{};
 };
 
+struct Config {
+    int theme;
+    std::wstring romDir;
+    std::wstring bgImage;
+};
+
 extern HWND g_hMain;
 extern HWND hBtnCheck;
 extern HWND hBtnFlash;
+extern HWND hBtnSettings;
 extern HWND hProgressBar;
 extern HWND hLog;
 extern HWND hLblStatus;
@@ -121,8 +134,12 @@ extern std::mutex g_LogMutex;
 extern std::deque<LogLine> g_LogQueue;
 extern std::atomic<bool> g_LogFlushPending;
 
+extern Config g_Config;
+
 void SafeDeleteObject(HGDIOBJ obj);
 std::wstring ToWide(const std::string& s);
+std::string WideToAnsi(const std::wstring& s);
+std::wstring ModuleDirW();
 std::wstring Win32ErrorText(DWORD err);
 bool FileExistsA(const std::string& path);
 std::string RomDir();
@@ -153,3 +170,9 @@ void CleanupGdi();
 std::vector<FlashStep> BuildFlashSteps();
 void CheckThread(uint32_t token);
 void FlashThread(uint32_t token);
+
+void LoadConfig();
+void SaveConfig();
+void ApplyTheme();
+void ShowSettingsDialog(HWND parent);
+void RefreshBrushes();
