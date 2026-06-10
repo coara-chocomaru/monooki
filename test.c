bc08fdeb894a37fe60207c2b7effeeabda2d6b9e
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-06-10 14:12:49 +0900

    Create test.c

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <binder/IServiceManager.h>
+#include <binder/ProcessState.h>
+#include <binder/IPCThreadState.h>
+
+using namespace android;
+
+int main() {
+    sp<IServiceManager> sm = defaultServiceManager();
+    sp<IBinder> service = sm->getService(String16("vendor.qti.hardware.systemhelper"));
+    if (service == nullptr) {
+        printf("failed to get service\n");
+        return -1;
+    }
+
+    Parcel data, reply;
+    data.writeInterfaceToken(String16("vendor.qti.hardware.systemhelper"));
+    data.writeInt32(0x41);
+    status_t status = service->transact(10, &data, &reply);
+    printf("transact returned: %d\n", status);
+
+    return 0;
+}
