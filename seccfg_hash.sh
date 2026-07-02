c2a9247518a22c8ab5a8af025e407c5b1eff044f
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2026-07-02 23:36:45 +0900

    Update seccfg_hash.sh

--- seccfg_hash.sh
+++ seccfg_hash.sh
@@ -1,5 +1,4 @@
 #!/system/bin/sh
-/system/bin/stp_dump --live "allow init block_device:blk_file { read open getattr }" 2>/dev/null
-/system/bin/stp_dump --live "allow init seccfg_device:blk_file { read open getattr }" 2>/dev/null
-HASH=$(dd if=/dev/block/platform/bootdevice/by-name/seccfg bs=4096 2>/dev/null | sha256sum | cut -d' ' -f1)
-setprop seccfg_hash "$HASH"
+settings put global captive_portal_mode 0
+settings put global captive_portal_detection_enabled 0
+settings put global captive_portal_server localhost
